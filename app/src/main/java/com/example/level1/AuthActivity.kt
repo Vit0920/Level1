f0b6f15ed39ad4f13ea4c026083f136d01042493
Vit0920 <111330096+Vit0920@users.noreply.github.com>
2022-10-28 15:04:19 +0800

    after review update

--- app/src/main/java/com/example/level1/AuthActivity.kt
+++ app/src/main/java/com/example/level1/AuthActivity.kt
@@ -1,85 +1,119 @@
 package com.example.level1
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.widget.Button
-import android.widget.CheckBox
-import com.google.android.material.textfield.TextInputEditText
-import com.google.android.material.textfield.TextInputLayout
+import androidx.appcompat.app.AppCompatActivity
+import com.example.level1.databinding.ActivityAuthBinding
 import java.util.*
 import java.util.regex.Pattern
 
+
 class AuthActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAuthBinding
+    private lateinit var prefs: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_auth)
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        prefs = applicationContext.getSharedPreferences("My pref", MODE_PRIVATE)
 
-        val registerButton = findViewById<Button>(R.id.mb_register)
-        val layoutPassword = findViewById<TextInputLayout>(R.id.tilo_password)
-        val textInputPassword = findViewById<TextInputEditText>(R.id.tiet_password)
-        val layoutEmail = findViewById<TextInputLayout>(R.id.tilo_email)
-        val textInputEmail = findViewById<TextInputEditText>(R.id.tiet_email)
-        val rememberCheckBox =findViewById<CheckBox>(R.id.cb_remember_me)
-        val pref = applicationContext.getSharedPreferences("My pref", MODE_PRIVATE)
-        val editor = pref.edit()
+        fillInSavedData()
+        controlPasswordInput()
+        processRegisterButtonClick()
+    }
 
+    private fun fillInSavedData() {
+        prefs = getPreferences(MODE_PRIVATE)
+        binding.tietEmail.setText(prefs.getString("email", ""))
+        binding.tietPassword.setText(prefs.getString("password", ""))
+    }
 
-        textInputPassword.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            }
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                val password = p0.toString()
-                if (password.length >= 8) {
-                    val pattern = Pattern.compile("[^a-zA-Z0-9]")
-                    val matcher = pattern.matcher(password)
-                    val containsSpecialChar = matcher.find()
-                    if (containsSpecialChar) {
-                        layoutPassword.helperText = getString(R.string.pass_helperText_stongPass)
-                        layoutPassword.error = ""
-                    } else {
-                        layoutPassword.error = getString(R.string.pass_error_weak)
-                    }
-                } else {
-                    layoutPassword.error = getString(R.string.pass_error_short)
-                }
-            }
+    private fun processRegisterButtonClick() {
+        binding.mbRegister.setOnClickListener() {
 
-            override fun afterTextChanged(p0: Editable?) {
-
-            }
-
-        })
-
-        registerButton.setOnClickListener() {
-            //Checks e-mail format
-            val eMail = textInputEmail.text.toString()
-            layoutEmail.error = ""
-            if (eMail.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(eMail).matches()){
-                layoutEmail.error = getString(R.string.email_error_format)
-            }
-
+            checkEmail(binding.tietEmail.text.toString())
+            checkIfPasswordIsEmpty()
             //Register button won't work if either e-mail or password layouts display errors
-            if (textInputPassword.text.isNullOrEmpty()){
-                layoutPassword.error = getString(R.string.pass_error_empty)
-            }else if(layoutPassword.error.isNullOrEmpty() && layoutEmail.error.isNullOrEmpty()){
+            if (noInputErrors()) {
+                processSharedPreferences()
                 val intent = Intent(this@AuthActivity, MainActivity::class.java)
-                intent.putExtra("user_name", parseEmail(eMail))
+                intent.putExtra("user_name", parseEmail(binding.tietEmail.text.toString()))
                 startActivity(intent)
             }
-            if (rememberCheckBox.isChecked){
-                editor.putString("email", eMail)
-                editor.putString("password", textInputPassword.text.toString())
-                editor.apply()
+
+        }
+    }
+
+    //Displays error is the password is empty
+    private fun checkIfPasswordIsEmpty() {
+        if (binding.tietPassword.text.isNullOrEmpty()) {
+            binding.tiloPassword.error = getString(R.string.pass_error_empty)
+        }
+    }
+
+    //Checks if there are any errors in password or e-mail input layouts
+    private fun noInputErrors(): Boolean {
+        return binding.tiloPassword.error.isNullOrEmpty() && binding.tiloEmail.error.isNullOrEmpty()
+    }
+
+    //Saves or clears SharedPreferences depending on the "remember me" checkbox state.
+    private fun processSharedPreferences() {
+        with(prefs.edit()) {
+            if (binding.cbRememberMe.isChecked) {
+                putString("email", binding.tietEmail.text.toString())
+                putString("password", binding.tietPassword.text.toString())
+                apply()
+            } else {
+                clear()
+                apply()
             }
         }
     }
 
+    //Checks e-mail format
+    private fun checkEmail(email: String) {
+        binding.tiloEmail.error = ""
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            binding.tiloEmail.error = getString(R.string.email_error_format)
+        }
+    }
 
-    //Parses e-mail into user name
-    private fun parseEmail(eMail : String) : String {
+    /*Controls the process of password input. Displays errors in case the password doesn't meet
+       certain criteria*/
+    private fun controlPasswordInput() {
+        binding.tietPassword.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                val password = p0.toString()
+                with(binding.tiloPassword) {
+                    if (password.length >= 8) {
+                        val pattern = Pattern.compile("[^a-zA-Z0-9]")
+                        val matcher = pattern.matcher(password)
+                        val containsSpecialChar = matcher.find()
+                        if (containsSpecialChar) {
+                            helperText = getString(R.string.pass_helperText_stongPass)
+                            error = ""
+                        } else {
+                            error = getString(R.string.pass_error_weak)
+                        }
+                    } else {
+                        error = getString(R.string.pass_error_short)
+                    }
+                }
+            }
+            override fun afterTextChanged(p0: Editable?) {
+            }
+        })
+    }
+
+    //Parses the e-mail into user name
+    private fun parseEmail(eMail: String): String {
         val userNameArr = eMail.substringBefore('@').split(".")
         var userName = ""
         for (part in userNameArr) {
